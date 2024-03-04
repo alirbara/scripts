8866c323cb6c10204ad41ae0030790e2a8d35cda
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2024-03-04 22:47:04 +0330

    feat: add install_gitlab_ubuntu.sh

---
+++ install_gitlab_ubuntu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+main() {
+    
+    sudo apt-get update
+    sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
+
+    # Email notification utility (Optional)
+    sudo apt-get install -y postfix
+    
+    curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+}
+
+main
