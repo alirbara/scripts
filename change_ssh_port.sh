ee24a70a5eee0b2dc2e798a21d7fdb90cf76ac82
Alireza Baratian <92058984+alirbara@users.noreply.github.com>
2024-05-12 22:36:13 +0330

    fix: add sudo

--- change_ssh_port.sh
+++ change_ssh_port.sh
@@ -2,9 +2,9 @@
 
 main() {
     read -p "Enter the new SSH port: " new_port
-    sed -i "s/^#Port.*/Port $new_port/" /etc/ssh/sshd_config
-    service sshd restart >> /dev/null
-    service ssh restart >> /dev/null
+    sudo sed -i "s/^#Port.*/Port $new_port/" /etc/ssh/sshd_config
+    sudo service sshd restart >> /dev/null
+    sudo service ssh restart >> /dev/null
     echo "SSH port changed to $new_port"
 }
 
