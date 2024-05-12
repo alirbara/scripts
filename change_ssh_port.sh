94cc9c4cb7a871603cc5465d0b20ab51d7126a44
Alireza Baratian <92058984+alirbara@users.noreply.github.com>
2024-05-12 19:28:08 +0330

    update ssh for ubuntu24

--- change_ssh_port.sh
+++ change_ssh_port.sh
@@ -3,7 +3,8 @@
 main() {
     read -p "Enter the new SSH port: " new_port
     sed -i "s/^#Port.*/Port $new_port/" /etc/ssh/sshd_config
-    service sshd restart
+    service sshd restart >> /dev/null
+    service ssh restart >> /dev/null
     echo "SSH port changed to $new_port"
 }
 
